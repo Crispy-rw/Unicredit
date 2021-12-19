c5564f58d33577a2d9f9c23e664b957d85e41b68
Crispy <nshimyumukizachristian@gmail.com>
2021-12-19 16:19:31 +0200

    Working Slider with Static Images

---
+++ core/src/main/java/com/academy/core/models/SliderModel.java
@@ -0,0 +1,42 @@
+package com.academy.core.models;
+
+import java.util.*;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Model(adaptables = Resource.class)
+public class SliderModel {
+    private static final Logger LOG = LoggerFactory.getLogger(SliderModel.class);
+
+    @SlingObject
+    private Resource currentResource;
+
+    public List<Map<String, String>> getSlides() {
+        List<Map<String, String>> slides = new ArrayList<>();
+        try {
+            Resource slidesList = currentResource.getChild("slidesList");
+            if (slidesList == null) {
+                return Collections.emptyList();
+            }
+            if (slidesList != null) {
+                for (Resource slide : slidesList.getChildren()) {
+                    // LOG.info(" DEBUG ==================><><><><><><><><>" + slide.getValueMap().get("fileReference", String.class));
+                    Map<String, String> slideMap = new HashMap<String, String>();
+                    slideMap.put("caption", slide.getValueMap().get("caption", String.class));
+                    slideMap.put("imageLocation", slide.getValueMap().get("fileReference", String.class));
+                    slides.add(slideMap);
+               }
+            }
+        } catch (Exception e) {
+            LOG.error("\n ERROR while getting Slide Details Multifield {} ", e.getMessage());
+        }
+        // LOG.error("\n Images Uploaded Successfully!");
+        return slides;
+    }
+}
